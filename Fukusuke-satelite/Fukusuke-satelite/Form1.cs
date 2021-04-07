8a2ee13cf3814b71897da08e0e7eff8d30b506a7
felipe silva <felipe.silvar85@gmail.com>
2021-04-06 20:21:34 -0400

    agregar login

--- Fukusuke-satelite/Fukusuke-satelite/Form1.cs
+++ Fukusuke-satelite/Fukusuke-satelite/Form1.cs
@@ -17,5 +17,14 @@ namespace Fukusuke_satelite
             InitializeComponent();
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
     }
 }
---
+++ Fukusuke-satelite/Fukusuke-satelite/Login.cs
@@ -0,0 +1,180 @@
+﻿
+namespace Fukusuke_satelite
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pblogo = new System.Windows.Forms.PictureBox();
+            this.pbnombre = new System.Windows.Forms.PictureBox();
+            this.lbUsuario = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btningresar = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pblogo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbnombre)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pblogo
+            // 
+            this.pblogo.Image = global::Fukusuke_satelite.Properties.Resources.sushi_derecha3;
+            this.pblogo.Location = new System.Drawing.Point(1, 12);
+            this.pblogo.Name = "pblogo";
+            this.pblogo.Size = new System.Drawing.Size(468, 302);
+            this.pblogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pblogo.TabIndex = 0;
+            this.pblogo.TabStop = false;
+            // 
+            // pbnombre
+            // 
+            this.pbnombre.Image = global::Fukusuke_satelite.Properties.Resources.FUKUSUKE;
+            this.pbnombre.Location = new System.Drawing.Point(67, 320);
+            this.pbnombre.Name = "pbnombre";
+            this.pbnombre.Size = new System.Drawing.Size(333, 73);
+            this.pbnombre.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pbnombre.TabIndex = 1;
+            this.pbnombre.TabStop = false;
+            // 
+            // lbUsuario
+            // 
+            this.lbUsuario.AutoSize = true;
+            this.lbUsuario.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbUsuario.ForeColor = System.Drawing.Color.DarkOrange;
+            this.lbUsuario.Location = new System.Drawing.Point(559, 28);
+            this.lbUsuario.Name = "lbUsuario";
+            this.lbUsuario.Size = new System.Drawing.Size(146, 20);
+            this.lbUsuario.TabIndex = 2;
+            this.lbUsuario.Text = "Nombre de Usuario";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(559, 60);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(243, 27);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(559, 165);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(243, 27);
+            this.textBox2.TabIndex = 5;
+            // 
+            // btningresar
+            // 
+            this.btningresar.BackColor = System.Drawing.Color.DarkOrange;
+            this.btningresar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btningresar.FlatAppearance.BorderColor = System.Drawing.Color.DarkOrange;
+            this.btningresar.FlatAppearance.BorderSize = 0;
+            this.btningresar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btningresar.Location = new System.Drawing.Point(559, 285);
+            this.btningresar.Name = "btningresar";
+            this.btningresar.Size = new System.Drawing.Size(243, 29);
+            this.btningresar.TabIndex = 6;
+            this.btningresar.Text = "Ingresar";
+            this.btningresar.UseVisualStyleBackColor = false;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.checkBox1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.checkBox1.ForeColor = System.Drawing.Color.DarkOrange;
+            this.checkBox1.Location = new System.Drawing.Point(559, 208);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(168, 24);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "Mostrar contraseña";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.DarkOrange;
+            this.label1.Location = new System.Drawing.Point(559, 129);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 20);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Contraseña";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkOrange;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(765, 363);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 29);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Salir";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(895, 415);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.btningresar);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lbUsuario);
+            this.Controls.Add(this.pbnombre);
+            this.Controls.Add(this.pblogo);
+            this.Name = "Form1";
+            this.Text = "FUKUSUKE";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pblogo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbnombre)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pblogo;
+        private System.Windows.Forms.PictureBox pbnombre;
+        private System.Windows.Forms.Label lbUsuario;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btningresar;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
